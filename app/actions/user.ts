659176916bf708d33d3470744e9181f6203de1c4
Suryadev78 <pandeysuryadev00@gmail.com>
2024-09-29 00:33:04 +0530

    learned server actions

--- app/actions/user.ts
+++ app/actions/user.ts
@@ -1,11 +1,17 @@
 "use server";
-import prisma from "@/app/db";
-import { error } from "console";
-export async function signup() {
-  try {
-    const res = await prisma.user.findFirst();
-    return res;
-  } catch (e) {
-    throw error("error: " + e);
-  }
+
+import client from "@/app/db";
+
+export async function signup(username: string, password: string) {
+  // should add zod validation here
+  const user = await client.user.create({
+    data: {
+      username: username,
+      password: password,
+    },
+  });
+
+  console.log(user.id);
+
+  return "Signed up!";
 }
--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,17 +1,34 @@
-import React from "react"; // Added import statement for React
+"use client";
+import { signup } from "@/app/actions/user";
+import React, { useState } from "react"; // Added import statement for React
 export default function SignIn() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [] = useState("");
   // Fixed typo in function name
-  <div>
-    <h1>Sign In</h1>
+  return (
     <div>
-      <label>Email:</label>
-      <input type="email" name="email" required />
-      <br />
-      <label>Password:</label>
-      <input type="password" name="password" required />
-      <br />
-      <input type="submit" value="Submit" />
-      <button>Submit</button>
+      <h1>Sign In</h1>
+      <div>
+        <label>Email:</label>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          name="email"
+          required
+        />
+        <br />
+        <label>Password:</label>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          name="password"
+          required
+        />
+        <br />
+        <input type="submit" value="Submit" />
+        <button onClick={() => signup(email, password)}>Submit</button>
+      </div>
     </div>
-  </div>;
+  );
 }
